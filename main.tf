d55c25ef1da00b3f02dcd08d8db63a810c1761b5
nishad <nishad@gmail.com>
2025-02-25 15:26:44 +0530

    varible added

--- main.tf
+++ main.tf
@@ -7,3 +7,10 @@ resource "aws_instance" "example"{
     instance_type = "t3.micro"
     
 }
+
+
+variable "instance" {
+    description = "this is an varible created for instance"
+    type = "string"
+  
+}
